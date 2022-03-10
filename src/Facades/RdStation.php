e7b28c57f0c19342d2a4e4da18ee437c243ec4fa
pedroni <pedronidegodois@hotmail.com>
2022-03-10 19:16:29 -0300

    typo

--- src/Facades/RdStation.php
+++ src/Facades/RdStation.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Facade;
 /**
  * @see \Pedroni\RdStation\RdStation
  * @method static \Pedroni\RdStation\Repositories\ContactRepository contacts()
- * @method static Pedroni\RdStation\Repositories\EventRepository events()
+ * @method static \Pedroni\RdStation\Repositories\EventRepository events()
  */
 class RdStation extends Facade
 {
